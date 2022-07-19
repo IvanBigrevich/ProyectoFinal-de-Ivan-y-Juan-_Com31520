4fed0755c13b37aaf0a26d129cb579b83326b1ab
Juan Pablo Salerno <juanpablo.salerno@hotmail.com>
2022-07-19 20:36:15 -0300

    Personas V 0. 0.4
    Prefab + estructura medieval

--- Assets/Script/SpawBrick.cs
+++ Assets/Script/SpawBrick.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpawBrick : MonoBehaviour
 {
+    float tiempo = 20;
+    float tiempoSpawnLadrillo;
+    public GameObject brick;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(brick,transform.position, transform.rotation);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnBrick();
+
     }
+
+    void spawnBrick()
+    { 
+        tiempoSpawnLadrillo -= Time.deltaTime;
+        if (tiempoSpawnLadrillo <= 0)
+        {
+            Instantiate(brick, transform.position, transform.rotation);
+            ResetSpawnBrickTime();
+        }
+    }
+    void ResetSpawnBrickTime()
+    {
+        tiempoSpawnLadrillo = tiempo;
+    }
+  
 }
