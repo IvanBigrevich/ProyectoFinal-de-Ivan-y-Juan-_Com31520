b8ab54da260459e75c05585ef71f6131507d5fc6
Juan Pablo Salerno <juanpablo.salerno@hotmail.com>
2022-08-06 18:02:24 -0300

    Menues V 0.0.1
    Menu Principal menu pausa canvas y objetos ed en pantalla

--- Assets/Script/UI/MainMenu/MainMenu.cs
+++ Assets/Script/UI/MainMenu/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
     }
     public void Play()
     {
-        SceneManager.LoadScene("Nivel 1");
+        SceneManager.LoadScene(1);
     }
 
     public void Exit()
