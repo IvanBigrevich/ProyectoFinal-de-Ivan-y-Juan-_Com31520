70efb5d8c61af32efaf756ce145bf83b4aa07669
IvanBigrevich <ivanbigrevich@gmail.com>
2022-08-11 16:52:42 -0300

    ScriptableObject(Carta)

---
+++ Assets/Script/Elements/Carta/Carta.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NuevaCarta", menuName = "Carta")]
+public class Carta : ScriptableObject
+{
+    public string nombreCarta;
+    public string descripcionCarta;
+    public Sprite dibujoCarta;
+    public int valorCarta;
+
+}
